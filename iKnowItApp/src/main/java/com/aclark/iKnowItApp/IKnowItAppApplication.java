32f5d65c79b2184a8d882e7efa48cc2fbdbfa144
Aranvihn Clark <aranvihn.clark@gmail.com>
2023-06-30 16:44:14 -0400

    Initial Commit

---
+++ iKnowItApp/src/main/java/com/aclark/iKnowItApp/IKnowItAppApplication.java
@@ -0,0 +1,13 @@
+package com.aclark.iKnowItApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IKnowItAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IKnowItAppApplication.class, args);
+	}
+
+}
---
+++ iKnowItApp/src/test/java/com/aclark/iKnowItApp/IKnowItAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aclark.iKnowItApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IKnowItAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
