78b331944b7b6517631eeea5b1829cefa3c16204
Aranvihn Clark <aranvihn.clark@gmail.com>
2023-07-05 13:33:08 -0400

    Added method to copy template section and posts.

---
+++ iKnowItApp/src/main/java/com/aclark/iKnowItApp/configuration/CopyFile.java
@@ -0,0 +1,16 @@
+package com.aclark.iKnowItApp.configuration;
+
+import java.io.*;
+
+public class CopyFile {
+
+    public static void copyFileUsingStream(File source, File dest) throws IOException {
+        try (InputStream is = new FileInputStream(source); OutputStream os = new FileOutputStream(dest)) {
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = is.read(buffer)) > 0) {
+                os.write(buffer, 0, length);
+            }
+        }
+    }
+}
